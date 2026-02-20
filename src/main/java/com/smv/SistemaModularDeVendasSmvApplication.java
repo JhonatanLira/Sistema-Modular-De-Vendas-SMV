81ecfbc4ca0f19141ef3b0cf3ce7f0a82c4c3b6c
Jhonatan <jhonatanlira@gmail.com>
2026-02-20 11:20:38 -0300

    inicio

---
+++ src/main/java/com/smv/SistemaModularDeVendasSmvApplication.java
@@ -0,0 +1,13 @@
+package com.smv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaModularDeVendasSmvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaModularDeVendasSmvApplication.class, args);
+	}
+
+}
