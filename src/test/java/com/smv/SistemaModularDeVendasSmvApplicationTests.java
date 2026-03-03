0b1ecce21cb48029c730b1ae27c5ef723d5a26a4
Jhonatan <jhonatanlira@gmail.com>
2026-03-03 15:13:20 -0300

    Inicio

---
+++ src/test/java/com/smv/SistemaModularDeVendasSmvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaModularDeVendasSmvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
