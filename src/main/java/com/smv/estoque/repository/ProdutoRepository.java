4a33b93e726922820504c8c7b7c5d3e671eda5f8
Jhonatan <jhonatanlira@gmail.com>
2026-03-03 15:53:03 -0300

    classe Produto e ProdutoRepository

---
+++ src/main/java/com/smv/estoque/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.smv.estoque.repository;
+
+import com.smv.estoque.domain.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
